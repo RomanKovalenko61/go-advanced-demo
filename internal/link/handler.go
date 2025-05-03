fe77af67bdf8abb10533e3ab9d1b74f0d0f4b7f6
Roman <astek14@mail.ru>
2025-05-03 11:39:59 +0300

    9.9 Check isExist link

--- internal/link/handler.go
+++ internal/link/handler.go
@@ -97,6 +97,11 @@ func (handler *LinkHandler) Delete() http.HandlerFunc {
 			http.Error(w, err.Error(), http.StatusBadRequest)
 			return
 		}
+		_, err = handler.LinkRepository.GetById(uint(id))
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusNotFound)
+			return
+		}
 		err = handler.LinkRepository.Delete(uint(id))
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)

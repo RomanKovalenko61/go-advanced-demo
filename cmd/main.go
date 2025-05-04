b7c69a97da1261eb177ab276fa8bf7ac430a5c74
Roman <astek14@mail.ru>
2025-05-04 08:20:58 +0300

    12.3 Context WithValue

--- cmd/main.go
+++ cmd/main.go
@@ -10,25 +10,18 @@ import (
 	"go/adv-demo/pkg/db"
 	"go/adv-demo/pkg/middleware"
 	"net/http"
-	"time"
 )
 
 func main() {
+	type key int
+	const EmailKey key = 0
 	ctx := context.Background()
-	ctxWithTimeout, cancel := context.WithTimeout(ctx, 4*time.Second)
-	defer cancel()
+	ctxWithValue := context.WithValue(ctx, EmailKey, "a@a.ru")
 
-	done := make(chan struct{})
-	go func() {
-		time.Sleep(3 * time.Second)
-		close(done)
-	}()
-
-	select {
-	case <-done:
-		fmt.Println("Done task")
-	case <-ctxWithTimeout.Done():
-		fmt.Println("Timeout")
+	if userEmail, ok := ctxWithValue.Value(EmailKey).(string); ok {
+		fmt.Println(userEmail)
+	} else {
+		fmt.Println("No value")
 	}
 }
 

889f77b443b673210e15838e10815e12821a602f
Roman <astek14@mail.ru>
2025-05-05 11:00:36 +0300

    14.4 Negative test

--- cmd/auth_test.go
+++ cmd/auth_test.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"encoding/json"
 	"go/adv-demo/internal/auth"
+	"io"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -24,5 +25,35 @@ func TestLoginSuccess(t *testing.T) {
 	}
 	if resp.StatusCode != http.StatusOK {
 		t.Fatalf("Expected %d got %d", http.StatusOK, resp.StatusCode)
+	}
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+	var respData auth.LoginResponse
+	err = json.Unmarshal(body, &respData)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if respData.Token == "" {
+		t.Fatal("Empty token")
+	}
+}
+
+func TestLoginFailed(t *testing.T) {
+	ts := httptest.NewServer(App())
+	defer ts.Close()
+
+	data, _ := json.Marshal(&auth.LoginRequest{
+		Email:    "a2@a.ru",
+		Password: "2",
+	})
+
+	resp, err := http.Post(ts.URL+"/auth/login", "application/json", bytes.NewReader(data))
+	if err != nil {
+		t.Fatal(err)
+	}
+	if resp.StatusCode != http.StatusUnauthorized {
+		t.Fatalf("Expected %d got %d", http.StatusUnauthorized, resp.StatusCode)
 	}
 }

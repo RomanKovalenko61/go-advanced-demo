9de0af9365520f3e3ae02cdbb57320516325a9c9
Roman <astek14@mail.ru>
2025-05-05 13:56:57 +0300

    14.12 Mock requests

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module go/adv-demo
 go 1.24.2
 
 require (
+	github.com/DATA-DOG/go-sqlmock v1.5.2
 	github.com/go-playground/validator/v10 v10.26.0
 	github.com/golang-jwt/jwt/v5 v5.2.2
 	github.com/joho/godotenv v1.5.1
---
+++ internal/auth/hansler_test.go
@@ -0,0 +1,59 @@
+package auth_test
+
+import (
+	"bytes"
+	"encoding/json"
+	"go/adv-demo/configs"
+	"go/adv-demo/internal/auth"
+	"go/adv-demo/internal/user"
+	"go/adv-demo/pkg/db"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/DATA-DOG/go-sqlmock"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func bootstrap() (*auth.AuthHandler, sqlmock.Sqlmock, error) {
+	database, mock, err := sqlmock.New()
+	if err != nil {
+		return nil, nil, err
+	}
+	gormDB, err := gorm.Open(postgres.New(postgres.Config{
+		Conn: database,
+	}))
+	if err != nil {
+		return nil, nil, err
+	}
+	userRepo := user.NewUserRepository(&db.Db{
+		DB: gormDB,
+	})
+	handler := auth.AuthHandler{
+		Config:      &configs.Config{Auth: configs.AuthConfig{Secret: "secret"}},
+		AuthService: auth.NewAuthService(userRepo),
+	}
+	return &handler, mock, nil
+}
+
+func TestLoginSuccess(t *testing.T) {
+	handler, mock, err := bootstrap()
+	rows := sqlmock.NewRows([]string{"email", "password"}).
+		AddRow("a2@a.ru", "$2a$10$vm6G8P1I8MwPNGIMXGejE.xQvaNmlHuf66MAKAQG1RQvhBjwgro0.")
+	mock.ExpectQuery("SELECT").WillReturnRows(rows)
+	if err != nil {
+		t.Fatal(err)
+	}
+	data, _ := json.Marshal(&auth.LoginRequest{
+		Email:    "a2@a.ru",
+		Password: "1",
+	})
+	reader := bytes.NewReader(data)
+	w := httptest.NewRecorder()
+	req := httptest.NewRequest(http.MethodPost, "/auth/login", reader)
+	handler.Login()(w, req)
+	if w.Code != http.StatusOK {
+		t.Errorf("got %d, expected %d", w.Code, http.StatusOK)
+	}
+}
